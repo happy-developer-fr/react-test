aa4a4067795c03864e6df959238a4b2663121d49
COTTET Julien <tetto@gmail.com>
2016-07-01 23:53:29 +0200

    ça fonctionne avec l'api swappi

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,102 +1,32 @@
+var rootUrl = "http://swapi.co/api/people";
 
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
+var Hero = React.createClass({
+  render: function(){
+    return (
+      <div> I am {this.props.name}</div>
+    )
+  }
+});
 
-var CommentList = React.createClass({
-  render: function() {
-      var commentNodes = this.props.data.map(function(comment) {
+var HeroList = React.createClass({
+  render: function(){
+    if(this.props.data.results){
+      var i = 0;
+      var heroes = this.props.data.results.map(function(hero){
         return (
-          <Comment author={comment.author} key={comment.id}>
-          {comment.text}
-          </Comment>
+          <Hero name={hero.name} key={i++}/>
         );
       });
-      return (
-      <div className="commentList">
-        {commentNodes}
-      </div>
-    );
-  }
-});
-
-
-var CommentForm = React.createClass({
-  getInitialState: function() {
-    return {author: '', text: ''};
-  },
-  handleAuthorChange: function(e) {
-    this.setState({author: e.target.value});
-  },
-  handleTextChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var author = this.state.author.trim();
-    var text = this.state.text.trim();
-    if (!text || !author) {
-      return;
     }
-    this.props.onCommentSubmit({author: author, text: text});
-    this.setState({author: '', text: ''});
-  },
-  render: function() {
-    return (
-      <form className="commentForm" onSubmit={this.handleSubmit}>
-      <input
-        type="text"
-        placeholder="Your name"
-        value={this.state.author}
-        onChange={this.handleAuthorChange}
-      />
-      <input
-        type="text"
-        placeholder="Say something..."
-        value={this.state.text}
-        onChange={this.handleTextChange}
-      />
-        <input type="submit" value="Post" />
-      </form>
-    );
+    return (<div class="HeroList">
+        {heroes}
+    </div>);
   }
 });
 
-// tutorial3.js
-
-var Comment = React.createClass({
-    rawMarkup: function() {
-    var md = new Remarkable();
-    var rawMarkup = md.render(this.props.children.toString());
-    return { __html: rawMarkup };
-  },
-  render: function() {
-    return (
-      <div className="comment" >
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
-      </div>
-    );
-  }
-});
-
-var CommentBox = React.createClass({
-  handleCommentSubmit: function(comment) {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: comment,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+var HeroBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
   },
   loadDocumentFromServer:function() {
     $.ajax({
@@ -111,25 +41,18 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
+  componentDidMount: function(){
     this.loadDocumentFromServer();
-    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
-  render: function() {
-    return (
-      <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.state.data} />
-        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
-      </div>
+  render: function(){
+    return(
+      <HeroList data={this.state.data}/>
     );
   }
 });
 
 ReactDOM.render(
-  <CommentBox url="/api/comments" pollInterval={2000}/>,
+  <HeroBox url="http://swapi.co/api/people"/>,
+
   document.getElementById('content')
 );
